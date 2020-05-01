512cc8a23e730943b07c4c56e2790b0659a949b6
Monika <monikawasilewska13@wp.pl>
2020-05-01 09:19:02 +0200

    first commit

--- src/components/home/Header.js
+++ src/components/home/Header.js
@@ -1,16 +1,15 @@
 import React from "react";
 import Hero from "../globals/Hero";
 import aboutImg from "../../images/aboutBcg.jpeg";
-import homeImg from "../../images/homeBcg.jpeg";
+import homeImg from "../../images/homeBcg.jpg";
 import Banner from "../globals/Banner";
 import { PrimaryBtn } from "../globals/Buttons";
 const Header = () => {
   return (
     <Hero img={homeImg}>
       <Banner
-        greeting="welcome to"
-        title="beachwalk resort"
-        text="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quasi provident eos sapiente deleniti in quisquam."
+        title="I am a web developer"
+        text="I make beautiful things"
       >
         <PrimaryBtn t="1rem">view details</PrimaryBtn>
         {/* <PrimaryBtn as="a" href="https://www.google.com">
